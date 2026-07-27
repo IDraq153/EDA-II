23510242d6ef789cdf3d85630a046ab83bf94475
darua <darioruamelendez@gmail.com>
2026-07-26 22:13:49 -0500

    p2_eliminar_mostrar

--- NIVEL5/eje6.cpp
+++ NIVEL5/eje6.cpp
@@ -8,6 +8,7 @@ template <typename T> struct NodoDoble {
 };
 
 template <typename T> void eliminarNodo(NodoDoble<T> *cabeza, T valorBuscar);
+template <typename T> void mostrarNodos(NodoDoble<T> *cabeza);
 
 int main() {
   cout << "ENLACES DOBLES" << std::endl;
@@ -20,26 +21,52 @@ int main() {
   n2->NodoAnt = n1;
   n3->NodoSgt = nullptr;
   n3->NodoAnt = n2;
+  n1->NodoAnt = nullptr;
 
   n1->item = 5;
   n2->item = 10;
   n3->item = 1;
-  eliminarNodo(n2, 10);
+  mostrarNodos(n1);
+  eliminarNodo(n1, 5);
+  cout << std::endl;
+  mostrarNodos(n2);
   return 0;
 }
 
 template <typename T> void eliminarNodo(NodoDoble<T> *cabeza, T valorBuscar) {
   bool flag = false;
+  NodoDoble<int> *cabeSgt = cabeza->NodoSgt;
   while (cabeza != nullptr && !flag) {
     if (cabeza->item == valorBuscar) {
       flag = true;
     } else {
+      cabeSgt = cabeSgt->NodoSgt;
       cabeza = cabeza->NodoSgt;
     }
   }
   if (!flag) {
     cout << "Item no encontrado";
   } else {
-    cout << cabeza->item << std::endl;
+    if (cabeza->NodoAnt == nullptr) {
+      cabeSgt->NodoAnt = nullptr;
+      delete cabeza;
+      cabeza = nullptr;
+    } else if (cabeza->NodoSgt == nullptr) {
+      cabeSgt = cabeza->NodoAnt;
+      delete cabeza;
+      cabeza = nullptr;
+      cabeSgt->NodoSgt = nullptr;
+    } else {
+      cabeza->NodoAnt->NodoSgt = cabeza->NodoSgt;
+      cabeza->NodoSgt->NodoAnt = cabeza->NodoAnt;
+      delete cabeza;
+      cabeza = nullptr;
+    }
+  }
+}
+template <typename T> void mostrarNodos(NodoDoble<T> *cabeza) {
+  while (cabeza != nullptr) {
+    cout << cabeza->item << '\t';
+    cabeza = cabeza->NodoSgt;
   }
 }
