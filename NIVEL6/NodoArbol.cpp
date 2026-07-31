3803237b912ba0de975a2dcc76013fe4be11c4cd
darua <darioruamelendez@gmail.com>
2026-07-30 19:09:08 -0500

    nodoArbolfix

--- NIVEL6/NodoArbol.cpp
+++ NIVEL6/NodoArbol.cpp
@@ -11,21 +11,21 @@ public:
     nodoIzq = nullptr;
     item = T{};
   }
-  nodoArbol(T item, nodoArbol<T> *nodoIzq, nodoArbol<T> *nodoDer) {
+  nodoArbol(T item, nodoArbol<T> *nodoIzq, nodoArbol<T> *nodoDer,
+            nodoArbol<T> *cabeza) {
     this->item = item;
     this->nodoIzq = nodoIzq;
     this->nodoDer = nodoDer;
+    this->cabeza = cabeza;
   }
 
-  bool arbolExiste() { return cabeza != nullptr; }
-
   T getItem() { return item; }
-  nodoArbol<T> getNodoIzq() { return nodoIzq; }
-  nodoArbol<T> getNodoDer() { return nodoDer; }
-  nodoArbol<T> getCabeza() { return cabeza; }
+  nodoArbol<T> *getNodoIzq() { return nodoIzq; }
+  nodoArbol<T> *getNodoDer() { return nodoDer; }
+  nodoArbol<T> *getCabeza() { return cabeza; }
 
   void setItem(T item) { this->item = item; }
-  void setNodoIzq(nodoArbol<T> nodo) { this->nodoIzq = nodo; }
-  void setNodoDer(nodoArbol<T> nodo) { this->nodoDer = nodo; }
-  void setCabeza(nodoArbol<T> cabeza) { this->cabeza = cabeza; }
+  void setNodoIzq(nodoArbol<T> *nodo) { this->nodoIzq = nodo; }
+  void setNodoDer(nodoArbol<T> *nodo) { this->nodoDer = nodo; }
+  void setCabeza(nodoArbol<T> *cabeza) { this->cabeza = cabeza; }
 };
