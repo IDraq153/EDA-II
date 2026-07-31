be9366a8036f3dea37416bb28f16ab00919ad3aa
darua <darioruamelendez@gmail.com>
2026-07-30 19:07:09 -0500

    nodoArbol

---
+++ NIVEL6/NodoArbol.cpp
@@ -0,0 +1,31 @@
+template <typename T> class nodoArbol {
+private:
+  T item;
+  nodoArbol<T> *nodoIzq;
+  nodoArbol<T> *nodoDer;
+  nodoArbol<T> *cabeza;
+
+public:
+  nodoArbol() {
+    nodoDer = nullptr;
+    nodoIzq = nullptr;
+    item = T{};
+  }
+  nodoArbol(T item, nodoArbol<T> *nodoIzq, nodoArbol<T> *nodoDer) {
+    this->item = item;
+    this->nodoIzq = nodoIzq;
+    this->nodoDer = nodoDer;
+  }
+
+  bool arbolExiste() { return cabeza != nullptr; }
+
+  T getItem() { return item; }
+  nodoArbol<T> getNodoIzq() { return nodoIzq; }
+  nodoArbol<T> getNodoDer() { return nodoDer; }
+  nodoArbol<T> getCabeza() { return cabeza; }
+
+  void setItem(T item) { this->item = item; }
+  void setNodoIzq(nodoArbol<T> nodo) { this->nodoIzq = nodo; }
+  void setNodoDer(nodoArbol<T> nodo) { this->nodoDer = nodo; }
+  void setCabeza(nodoArbol<T> cabeza) { this->cabeza = cabeza; }
+};
