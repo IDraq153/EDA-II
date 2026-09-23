ed1a585cbcf0366fd9bfc8201aa986c7efc01ce3
darua <darioruamelendez@gmail.com>
2026-09-23 17:16:43 -0500

    explic_hash

--- CLASES_EDAII/SEM5/ARREGLOS/HASHTABLE/tablaHash.cpp
+++ CLASES_EDAII/SEM5/ARREGLOS/HASHTABLE/tablaHash.cpp
@@ -18,13 +18,19 @@ public:
   int funcionHash(int k) { return k % tam; }
 
   void insertar(int k) {
+    // Esta funcion tiene como objetivo inserar un elemento
+    // primero saca el indice mediante el modulo del valor ingresado
     int intentos = 0;
     int pos = funcionHash(k);
 
+    // Tenemos como condiciones el arreglo vacio y cuando se cumple un ciclo
     while (tabla[pos] != -1 && intentos < tam) {
       pos = funcionHash(pos + 1);
       intentos++;
     }
+
+    // Si encuentra un lugar vacio esa sera la posicion en la que ira nuestro
+    // elemento
     if (intentos < tam) {
       tabla[pos] = k;
     } else {
